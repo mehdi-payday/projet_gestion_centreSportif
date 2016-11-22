509131e0241dd0bb675bd1c49185715ec38d4c9e
Mehdi Hamidi <Mohamed.hamidi.1@collegeahuntsic.qc.ca>
2016-11-22 10:51:30 -0500

    Initial commit, added sqlite ref and .gitignore

---
+++ projet_gestion_centreSportif/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="projet_gestion_centreSportif.Global" Language="C#" %>
---
+++ projet_gestion_centreSportif/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(projet_gestion_centreSportif.Startup))]
+namespace projet_gestion_centreSportif
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
