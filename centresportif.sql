ccf765403a27347c2f75b171126197d96a6bd46b
Mehdi Hamidi <Mohamed.hamidi.1@collegeahuntsic.qc.ca>
2016-11-23 19:41:44 -0500

    Added: full database schema

---
+++ centresportif.sql
@@ -0,0 +1,141 @@
+-- phpMyAdmin SQL Dump
+-- version 4.2.11
+-- http://www.phpmyadmin.net
+--
+-- Client :  127.0.0.1
+-- Généré le :  Mer 23 Novembre 2016 à 19:51
+-- Version du serveur :  5.6.21
+-- Version de PHP :  5.6.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de données :  `centresportif`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `activite`
+--
+
+CREATE TABLE IF NOT EXISTS `activite` (
+`id` int(11) NOT NULL,
+  `nom` varchar(50) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `prix` double NOT NULL,
+  `duree` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `inscription`
+--
+
+CREATE TABLE IF NOT EXISTS `inscription` (
+  `id` int(11) DEFAULT NULL,
+  `idMembre` int(11) NOT NULL,
+  `idActivite` int(11) NOT NULL,
+  `date_debut` date NOT NULL,
+  `date_fin` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `membre`
+--
+
+CREATE TABLE IF NOT EXISTS `membre` (
+  `id` int(11) NOT NULL,
+  `first_name` varchar(20) DEFAULT NULL,
+  `last_name` varchar(20) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `isAdmin` int(1) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `membre`
+--
+
+INSERT INTO `membre` (`id`, `first_name`, `last_name`, `email`, `password`, `isAdmin`) VALUES
+(1, 'admin', 'admin', 'admin@site', 'D033E22AE348AEB5660FC2140AEC35850C4DA997', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `visite`
+--
+
+CREATE TABLE IF NOT EXISTS `visite` (
+  `id` int(11) DEFAULT NULL,
+  `idMembre` int(11) NOT NULL,
+  `DATE` datetime DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Index pour les tables exportées
+--
+
+--
+-- Index pour la table `activite`
+--
+ALTER TABLE `activite`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `inscription`
+--
+ALTER TABLE `inscription`
+ ADD KEY `idMembre` (`idMembre`), ADD KEY `idActivite` (`idActivite`);
+
+--
+-- Index pour la table `membre`
+--
+ALTER TABLE `membre`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `visite`
+--
+ALTER TABLE `visite`
+ ADD KEY `idMembre` (`idMembre`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `activite`
+--
+ALTER TABLE `activite`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Contraintes pour les tables exportées
+--
+
+--
+-- Contraintes pour la table `inscription`
+--
+ALTER TABLE `inscription`
+ADD CONSTRAINT `inscription_ibfk_1` FOREIGN KEY (`idMembre`) REFERENCES `membre` (`id`),
+ADD CONSTRAINT `inscription_ibfk_2` FOREIGN KEY (`idActivite`) REFERENCES `activite` (`id`);
+
+--
+-- Contraintes pour la table `visite`
+--
+ALTER TABLE `visite`
+ADD CONSTRAINT `visite_ibfk_1` FOREIGN KEY (`idMembre`) REFERENCES `membre` (`id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
