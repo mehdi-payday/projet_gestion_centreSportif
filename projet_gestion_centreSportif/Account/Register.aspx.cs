73ab2c1f2853a725f23dac4efc4e5c5a94d55d12
Mehdi Hamidi <Mohamed.hamidi.1@collegeahuntsic.qc.ca>
2016-11-26 18:09:05 -0500

    Added: rest of the crud in MembreService

--- projet_gestion_centreSportif/Account/Register.aspx.cs
+++ projet_gestion_centreSportif/Account/Register.aspx.cs
@@ -1,18 +1,12 @@
 ﻿using System;
-using System.Linq;
-using System.Web;
 using System.Web.UI;
-using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.Owin;
-using Owin;
 using projet_gestion_centreSportif.Models;
 using System.Web.Security;
 using projet_gestion_centreSportif.Services;
 
 namespace projet_gestion_centreSportif.Account
 {
-    public partial class Register : Page
-    {
+    public partial class Register : Page {
         protected void CreateUser_Click(object sender, EventArgs e) {
             Membre membre = new Membre();
             membre.Prenom = First_name.Text;
@@ -20,12 +14,10 @@ namespace projet_gestion_centreSportif.Account
             membre.Email = Email.Text;
             membre.Password = FormsAuthentication.HashPasswordForStoringInConfigFile(Password.Text, "SHA1");
             MembreService membreService = new MembreService();
-            if (membreService.inscrire(membre)) {
-                FormsAuthentication.SetAuthCookie(Email.Text, false);
-                FormsAuthentication.RedirectFromLoginPage(Email.Text, false);
-            } else {
-                ErrorMessage.Text = "Failed to register user, try again!";
-            }
+            membreService.inscrire(membre);
+            FormsAuthentication.SetAuthCookie(Email.Text, false);
+            FormsAuthentication.RedirectFromLoginPage(Email.Text, false);
+            
         }
     }
 }
--- projet_gestion_centreSportif/Services/MembreService.cs
+++ projet_gestion_centreSportif/Services/MembreService.cs
@@ -5,51 +5,66 @@ using System.Collections.Generic;
 
 namespace projet_gestion_centreSportif.Services {
     public class MembreService {
-        Connection connection;
+        Connection connexion;
         private static readonly string INSERT_MEMBRE_QUERY = "INSERT INTO membre(`prenom`, `nom`, `email`, `password`) VALUES(@prenom, @nom, @email, @password)";
+        private static readonly string READ_MEMBRE_QUERY = "SELECT `id`, `prenom`, `nom`, `email`, `password`, `isAdmin` FROM membre WHERE `id`=@id";
+        private static readonly string UPDATE_MEMBRE_QUERY = "UPDATE membre SET `prenom`=@prenom, `nom`=@nom, `email`=@email, `password`=@password WHERE `id`=@id";
+        private static readonly string DELETE_MEMBRE_QUERY = "DELETE FROM membre WHERE `id`=@id";
+        private static readonly string GET_ALL_MEMRE_QUERY = "SELECT `id`, `prenom`, `nom`, `email`,`isAdmin` FROM membre";
         private static readonly string LOGIN_MEMBRE_QUERY = "SELECT * FROM membre WHERE email=@email and password=@password";
-        private static readonly string GET_ALL_MEMRES_QUERY = "SELECT `id`, `prenom`, `nom`, `email`,`isAdmin` FROM membre";
 
         public MembreService() {
-            connection = new Connection();
+            connexion = new Connection();
         }
 
-        public Boolean inscrire(Membre membre) {
-            Boolean inscrit = true;
+        /// <summary>
+        /// Ajouter un membre a la base de donnee
+        /// </summary>
+        /// <param name="membre">le membre a ajouter</param>
+        public void add(Membre membre) {
             try {
+                connexion.Open();
+                using (MySqlCommand command = new MySqlCommand(MembreService.INSERT_MEMBRE_QUERY, connexion.getConnection())) {
+                    command.Prepare();
+                    command.Parameters.AddWithValue("prenom", membre.Prenom);
+                    command.Parameters.AddWithValue("nom", membre.Nom);
+                    command.Parameters.AddWithValue("email", membre.Email);
+                    command.Parameters.AddWithValue("password", membre.Password);
 
-                MySqlCommand command;
-                connection.Open();
-                command = new MySqlCommand(MembreService.INSERT_MEMBRE_QUERY, connection.getConnection());
-                command.Prepare();
-                command.Parameters.AddWithValue("prenom", membre.Prenom);
-                command.Parameters.AddWithValue("nom", membre.Nom);
-                command.Parameters.AddWithValue("email", membre.Email);
-                command.Parameters.AddWithValue("password", membre.Password);
-
-                command.ExecuteNonQuery();
-                connection.Close();
+                    command.ExecuteNonQuery();
+                }
+                connexion.Close();
             } catch (MySqlException mysqlException) {
-                inscrit = false;
                 Console.WriteLine(mysqlException.Message);
             }
-            return inscrit;
         }
+        /// <summary>
+        /// Inscrir un membre.
+        /// </summary>
+        /// <param name="membre">le membre a inscrir</param>
+        public void inscrire(Membre membre) {
+            add(membre);
+        }
+        /// <summary>
+        /// Verifier si la combinaison de mot de passe et email est valide
+        /// </summary>
+        /// <param name="membre">le membre a verifier</param>
+        /// <returns>true ou false</returns>
         public Boolean MembreValid(Membre membre) {
             Boolean valid = false;
             try {
-                MySqlCommand command;
-                connection.Open();
-                command = new MySqlCommand(MembreService.LOGIN_MEMBRE_QUERY, connection.getConnection());
-                command.Prepare();
+                connexion.Open();
+                using (MySqlCommand command = new MySqlCommand(MembreService.LOGIN_MEMBRE_QUERY, connexion.getConnection())) {
+                    command.Prepare();
 
-                command.Parameters.AddWithValue("email", membre.Email);
-                command.Parameters.AddWithValue("password", membre.Password);
+                    command.Parameters.AddWithValue("email", membre.Email);
+                    command.Parameters.AddWithValue("password", membre.Password);
 
-                if (command.ExecuteScalar() != null && (int)command.ExecuteScalar() == 1) {
-                    valid = true;
+                    if (command.ExecuteScalar() != null && (int)command.ExecuteScalar() == 1) {
+                        valid = true;
+                    }
                 }
-                connection.Close();
+                connexion.Close();
             } catch (MySqlException mysqlException) {
                 Console.WriteLine(mysqlException.Message);
             }
@@ -57,23 +72,104 @@ namespace projet_gestion_centreSportif.Services {
             return valid;
         }
 
+        /// <summary>
+        /// Read un membre.
+        /// </summary>
+        /// <param name="idMembre">l'id du membre</param>
+        /// <returns>un membre; null sinon</returns>
+        public Membre Read(int idMembre) {
+            Membre membre = null;
+            try {
+                connexion.Open();
+                using (MySqlCommand command = new MySqlCommand(MembreService.READ_MEMBRE_QUERY, connexion.getConnection())) {
+                    command.Prepare();
+                    command.Parameters.AddWithValue("id", idMembre);
+                    using (MySqlDataReader reader = command.ExecuteReader()) {
+                        if (reader.Read()) {
+                            membre = new Membre();
+                            membre.Id = reader.GetInt16("id");
+                            membre.Prenom = reader.GetString("prenom");
+                            membre.Nom = reader.GetString("nom");
+                            membre.Password = reader.GetString("password");
+                            membre.IsAdmin = reader.GetInt16("isAdmin");
+                        }
+                    }
+                }
+                connexion.Close();
+            } catch (MySqlException mysqlException) {
+                Console.WriteLine(mysqlException.Message);
+            }
+            return membre;
+        }
+        /// <summary>
+        /// Fait un Update dans la BD sur la table Membre
+        /// </summary>
+        /// <param name="membre">Le membre a modifier</param>
+        public void Update(Membre membre) {
+            try {
+                connexion.Open();
+                using (MySqlCommand command = new MySqlCommand(MembreService.UPDATE_MEMBRE_QUERY, connexion.getConnection())) {
+                    command.Prepare();
+                    command.Parameters.AddWithValue("prenom", membre.Prenom);
+                    command.Parameters.AddWithValue("nom", membre.Nom);
+                    command.Parameters.AddWithValue("email", membre.Email);
+                    command.Parameters.AddWithValue("password", membre.Password);
+
+                    command.ExecuteNonQuery();
+                }
+                connexion.Close();
+            } catch (MySqlException exception) {
+                // TODO
+            }
+        }
+
+        /// <summary>
+        /// Supprimer un membre.
+        /// </summary>
+        /// <param name="idMembre"> id du membre a supprimer</param>
+        public void Delete(int idMembre) {
+            try {
+                connexion.Open();
+                using (MySqlCommand command = new MySqlCommand(MembreService.DELETE_MEMBRE_QUERY, connexion.getConnection())) {
+                    command.Prepare();
+                    command.Parameters.AddWithValue("id", idMembre);
+
+                    command.ExecuteNonQuery();
+                }
+                connexion.Close();
+            } catch (MySqlException exception) {
+                // TODO
+            }
+        }
+        /// <summary>
+        /// Desinscrire un membre.
+        /// </summary>
+        /// <param name="idMembre"> id du membre a Desinscrire</param>
+        public void Desinscrire(int idMembre) {
+            Delete(idMembre);
+        }
+
+        /// <summary>
+        /// Retourne la liste de tous les membres.
+        /// </summary>
+        /// <returns>La liste de toute les membres; une liste vide sinon</returns>
         public List<Membre> GetALl() {
             List<Membre> membres = null;
             try {
-                MySqlCommand command;
-                connection.Open();
-                command = new MySqlCommand(MembreService.GET_ALL_MEMRES_QUERY, connection.getConnection());
-                MySqlDataReader reader = command.ExecuteReader();
-                while (reader.Read()) {
-                    Membre membre = new Membre();
-                    membre.Id = reader.GetInt16(0);
-                    membre.Prenom = reader.GetString(2);
-                    membre.Nom = reader.GetString(1);
-                    membre.Email = reader.GetString(3);
-                    membre.IsAdmin = reader.GetInt16(4);
-                    membres.Add(membre);
+                connexion.Open();
+                using (MySqlCommand command = new MySqlCommand(MembreService.GET_ALL_MEMRE_QUERY, connexion.getConnection())) {
+                    MySqlDataReader reader = command.ExecuteReader();
+                    while (reader.Read()) {
+                        Membre membre = new Membre();
+                        membre.Id = reader.GetInt16("id");
+                        membre.Prenom = reader.GetString("prenom");
+                        membre.Nom = reader.GetString("nom");
+                        membre.Email = reader.GetString("Email");
+                        membre.IsAdmin = reader.GetInt16("isAdmin");
+                        membres.Add(membre);
+                    }
                 }
-                connection.Close();
+                connexion.Close();
             } catch (MySqlException mysqlException) {
                 Console.WriteLine(mysqlException.Message);
             }
